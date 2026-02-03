0df460a27ae93888fbe9384e899bff18402d45a6
Pedrassa <49522692+Pedrassa@users.noreply.github.com>
2026-02-03 17:13:47 +0000

    First implementation

---
+++ calculator.py
@@ -0,0 +1,9 @@
+# calculator.py
+
+def add(x, y):
+    print(f"\n--- DEBUG: Calculating {x} + {y} ---")
+    return x + y
+
+def subtract(x, y):
+    print(f"\n--- DEBUG: Calculating {x} - {y} ---")
+    return x - y
